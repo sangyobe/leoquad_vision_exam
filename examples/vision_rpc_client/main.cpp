5b7391b000e1b9568df33b823220d0f5e450ca65
Sangyup Yi <sean.yi@hyundai.com>
2024-02-26 17:59:47 +0900

    twick data transfer rate.

--- examples/vision_rpc_client/main.cpp
+++ examples/vision_rpc_client/main.cpp
@@ -23,13 +23,14 @@ int main()
     std::unique_ptr<RpcClient> rpcClient = std::make_unique<RpcClient>("localhost:50052");
 
     double t_ = 0.0;
-    double dt_ = 0.001;
+    double dt_ = 0.01;
     while (t_ < 10.0) {
         rpcClient->template StartCall<RequestOdometryCall>((void*)(&odomEmul.odom));
+        // rpcClient->template StartCall<NotifySteppableAreaCall>((void*)(&steppablesEmul.steppables));
         std::this_thread::sleep_for(std::chrono::milliseconds(long(dt_ * 1000)));
         t_ += dt_;
     }
-    // rpcClient->template StartCall<NotifySteppableAreaCall>((void*)(&steppablesEmul.steppables));
+    
     // rpcClient->template StartCall<StreamOdometryCall>((void*)(&odomEmul.odom));
     // rpcClient->template StartCall<StreamSteppableAreaCall>((void*)(&steppablesEmul.steppables));
 
